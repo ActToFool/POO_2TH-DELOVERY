9925b621c45278ae8208749bf20e78fb1e08196d
Emanuel Álvarez <Emanuel lvarez@10.150.21.115>
2018-10-17 11:59:41 -0500

    Punto 1: Falta crear los productos y monedas en la clase GestionProducto

--- Vending_2th-delivery/src/Control/GestionProducto.java
+++ Vending_2th-delivery/src/Control/GestionProducto.java
@@ -5,10 +5,32 @@
  */
 package Control;
 
+import Entity.Moneda;
+import Entity.Producto;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Emanuel Álvarez
  */
 public class GestionProducto {
+
+    public Map<String, Producto> crearProductos(){
+        Map<String, Producto>productos = new HashMap<>();
+        //Hay que crear los productis
+        return productos;
+    }
+    
+    public ArrayList<Moneda> dineroAcumulado(){
+        ArrayList<Moneda> dinero = new ArrayList<>();
+        //Hay que crear las monedas
+        return dinero;
+    }
+    
+    //CONSTRUCTORS
+    public GestionProducto() {
+    }
     
 }
--- Vending_2th-delivery/src/Control/Vending.java
+++ Vending_2th-delivery/src/Control/Vending.java
@@ -19,12 +19,16 @@ public class Vending {
     private GestionProducto gestion;
     private Venta ventaActual;
     private ArrayList<Venta> ventasRealizadas;
+    private ArrayList<Moneda> dineroAcumulado;
+    private Map<String, Producto> Catalogo;
 
     //CONSTRUCTOR
     public Vending() {
         this.gestion = new GestionProducto();
         this.ventaActual = new Venta();
         this.ventasRealizadas = new ArrayList<>();
+        this.Catalogo = this.gestion.crearProductos();
+        this.dineroAcumulado = this.gestion.dineroAcumulado();
     }
 
     
@@ -53,12 +57,20 @@ public class Vending {
         this.ventasRealizadas = ventasRealzadas;
     }
 
-    public Moneda getDineroAcumulado() {
+    public ArrayList<Moneda> getDineroAcumulado() {
         return dineroAcumulado;
     }
 
-    public void setDineroAcumulado(Moneda dineroAcumulado) {
+    public void setDineroAcumulado(ArrayList<Moneda> dineroAcumulado) {
         this.dineroAcumulado = dineroAcumulado;
+    }
+
+    public ArrayList<Venta> getVentasRealizadas() {
+        return ventasRealizadas;
+    }
+
+    public void setVentasRealizadas(ArrayList<Venta> ventasRealizadas) {
+        this.ventasRealizadas = ventasRealizadas;
     }
 
     public Map<String, Producto> getCatalogo() {
@@ -68,6 +80,5 @@ public class Vending {
     public void setCatalogo(Map<String, Producto> Catalogo) {
         this.Catalogo = Catalogo;
     }
-    private Moneda dineroAcumulado;
-    private Map<String, Producto> Catalogo;
+    
 }
