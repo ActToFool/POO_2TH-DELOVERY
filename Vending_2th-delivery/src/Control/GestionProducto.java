21a1f1fc017aabbfcb7d689b06ca24c8f937a805
Emanuel Álvarez <Emanuel lvarez@10.150.21.115>
2018-10-17 21:29:42 -0500

    nothing

--- Vending_2th-delivery/src/Control/GestionProducto.java
+++ Vending_2th-delivery/src/Control/GestionProducto.java
@@ -6,7 +6,11 @@
 package Control;
 
 import Entity.Moneda;
+import Entity.Adicional;
+import Entity.Alcoholica;
+import Entity.Alimento;
 import Entity.Producto;
+import Entity.SinAlcohol;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,13 +23,20 @@ public class GestionProducto {
 
     public Map<String, Producto> crearProductos(){
         Map<String, Producto>productos = new HashMap<>();
-        //Hay que crear los productis
+        ArrayList<Producto> producto_obs=new ArrayList<>();
+        Map<String, Adicional> adicional_aux=new HashMap<>();
+        
+        //Se crean obsequios para el primer producto
+        Producto o1 = new SinAlcohol(2, 1, 1000, "101", 50);
+        Producto o2 = new Alcoholica(5, 1, 3000, "102", 30);
+        Producto o3 = new Alimento(100, 10, "103", 0);
         return productos;
     }
     
     public ArrayList<Moneda> dineroAcumulado(){
         ArrayList<Moneda> dinero = new ArrayList<>();
         //Hay que crear las monedas
+             
         return dinero;
     }
     
